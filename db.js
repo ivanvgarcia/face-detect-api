7aaf7190fa33c05fd7f1b5040ee390c80f20e7d4
Ivan <ivanvlora@gmail.com>
2019-04-19 18:38:42 +0900

    modified dp when on prod or local env

--- db.js
+++ db.js
@@ -1,13 +1,26 @@
 const knex = require("knex");
+const environment = process.env.NODE_ENV || "development";
 
-const postgres = knex({
-  client: "pg",
-  connection: {
-    host: "postgresql-convex-89162",
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-  }
-});
+let postgres;
+
+if (environment === "development") {
+  postgres = knex({
+    client: "pg",
+    connection: {
+      host: "127.0.0.1",
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
+    }
+  });
+} else {
+  postgres = knex({
+    client: "pg",
+    connection: {
+      host: process.env.DATABASE_URL,
+      ssl: true
+    }
+  });
+}
 
 module.exports = postgres;
