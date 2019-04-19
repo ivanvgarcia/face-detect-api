718b4bbf702dee8fdf948d2561918f64e8adcf02
Ivan <ivanvlora@gmail.com>
2019-04-19 18:31:59 +0900

    updated db connection to heroku

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const knex = require("knex");
 const postgres = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
+    host: "postgresql-convex-89162",
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
