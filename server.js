2119306ab9f877d63f1ef0780ad47f0e7106bd2e
Ivan <ivanvlora@gmail.com>
2019-04-19 18:10:32 +0900

    added a port env var

--- server.js
+++ server.js
@@ -20,6 +20,8 @@ app.get("/", (req, res) => {
   res.status(200).json(db.users);
 });
 
-app.listen(4000, () => {
-  console.log("Server is Running!");
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server is Running on port ${PORT}`);
 });
